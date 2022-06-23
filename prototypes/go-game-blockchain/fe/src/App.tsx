9905ee5f05d76e3bb1d1faeaae1e9ca97dbaeac3
Frederick <fredred@ntu.im>
2022-06-23 21:14:59 +0800

    prettier

--- prototypes/go-game-blockchain/fe/src/App.tsx
+++ prototypes/go-game-blockchain/fe/src/App.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
-import './App.css';
-import Square from './components/Square'
+import "./App.css";
+import Square from "./components/Square";
 
 function App() {
   const [board, setBoard] = useState(Array(19).fill(Array(19).fill(null)));
@@ -12,14 +12,14 @@ function App() {
 
   const updateBoard = (y: number, x: number, newValue: string) => {
     //copy a new board and assign the new value
-    setBoard((board: Array<Array<string>>) => 
-    board.map((row, currentY) => {
-      if (currentY !== y) return row;
-      return row.map((col, currentX) => {
-        if (currentX !== x) return col;
-        return newValue;
-      });
-    })
+    setBoard((board: Array<Array<string>>) =>
+      board.map((row, currentY) => {
+        if (currentY !== y) return row;
+        return row.map((col, currentX) => {
+          if (currentX !== x) return col;
+          return newValue;
+        });
+      })
     );
   };
 
@@ -29,27 +29,30 @@ function App() {
     // lastRow.current = row;
     // lastCol.current = col;
 
-    updateBoard(row, col, isBlackNext.current ? 'black' : 'white')
+    updateBoard(row, col, isBlackNext.current ? "black" : "white");
 
     isBlackNext.current = !isBlackNext.current; //switch turns
-  }
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       {/* Wrapper */}
-      <div className="flex text-center my-[100px] mx-auto"> 
+      <div className='flex text-center my-[100px] mx-auto'>
         {/* Board */}
-        <div className="my-0 mx-auto">
+        <div className='my-0 mx-auto'>
           {board.map((row: Array<string>, rowIndex: number) => {
             return (
-              <div className="flex">
-                {
-                  row.map((col: string, colIndex: number) => {
-                    return (
-                        <Square row={rowIndex} col={colIndex} val={col} onClick={handlePieceClick}/>
-                    );
-                  })
-                }
+              <div className='flex'>
+                {row.map((col: string, colIndex: number) => {
+                  return (
+                    <Square
+                      row={rowIndex}
+                      col={colIndex}
+                      val={col}
+                      onClick={handlePieceClick}
+                    />
+                  );
+                })}
               </div>
             );
           })}
--- prototypes/go-game-blockchain/fe/src/components/Square.tsx
+++ prototypes/go-game-blockchain/fe/src/components/Square.tsx
@@ -3,19 +3,32 @@ type Props = {
   col?: number;
   val?: string;
   onClick: Function;
-}
+};
 
 const Square = (props: Props) => {
-  const beforeStyle = `before:content-[''] before:w-[2px] before:bg-black before:absolute before:left-1/2 before:-translate-x-1/2 ${props.row === 0 ? 'before:top-1/2' : 'before:top-0'} ${props.row === 18 ? 'before:h-1/2' : 'before:h-full'} `
-  const afterStyle = `after:content-[''] after:h-[2px] after:bg-black after:absolute after:top-1/2 after:-translate-y-1/2 ${props.col === 0 ? 'after:left-1/2' : 'after:left-0'} ${props.col === 18 ? 'after:w-1/2' : 'after:w-full'} `
+  const beforeStyle = `before:content-[''] before:w-[2px] before:bg-black before:absolute before:left-1/2 before:-translate-x-1/2 ${
+    props.row === 0 ? "before:top-1/2" : "before:top-0"
+  } ${props.row === 18 ? "before:h-1/2" : "before:h-full"} `;
+  const afterStyle = `after:content-[''] after:h-[2px] after:bg-black after:absolute after:top-1/2 after:-translate-y-1/2 ${
+    props.col === 0 ? "after:left-1/2" : "after:left-0"
+  } ${props.col === 18 ? "after:w-1/2" : "after:w-full"} `;
   const handleSquareClick = () => {
-    props.onClick(props.row, props.col, props.val)
-  }
+    props.onClick(props.row, props.col, props.val);
+  };
   return (
     // Cell
-    <div className={'bg-[#ba8c63] w-[30px] h-[30px] relative ' + beforeStyle + afterStyle}>
+    <div
+      className={
+        "bg-[#ba8c63] w-[30px] h-[30px] relative " + beforeStyle + afterStyle
+      }
+    >
       {/* Piece */}
-      <div className={`w-full h-full rounded-[50%] absolute scale-50 z-50 ${props.val === 'white' ? 'bg-white' : ''} ${props.val === 'black' ? 'bg-black' : ''}`} onClick={handleSquareClick}/>
+      <div
+        className={`w-full h-full rounded-[50%] absolute scale-50 z-50 ${
+          props.val === "white" ? "bg-white" : ""
+        } ${props.val === "black" ? "bg-black" : ""}`}
+        onClick={handleSquareClick}
+      />
     </div>
   );
 };
