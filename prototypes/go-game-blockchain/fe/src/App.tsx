e2b8a84f8981513233b88519040b3e98163e4de8
pinyuho <pollyho42@gmail.com>
2022-06-23 10:39:27 +0800

    fix import out of src

--- prototypes/go-game-blockchain/fe/src/App.tsx
+++ prototypes/go-game-blockchain/fe/src/App.tsx
@@ -1,9 +1,33 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import './App.css';
 import Square from './components/Square'
+import { useMoralis } from "react-moralis"
 
 function App() {
   const [board, setBoard] = useState(Array(19).fill(Array(19).fill(null)));
+  const { enableWeb3, isWeb3Enabled, isWeb3EnableLoading, account, Moralis, deactivateWeb3 } = useMoralis()
+  useEffect(() => {
+    if (isWeb3Enabled) return
+    if (typeof window !== "undefined") {
+        if (window.localStorage.getItem("connected")) {
+            enableWeb3()
+            // enableWeb3({provider: window.localStorage.getItem("connected")}) // add walletconnect
+        }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [isWeb3Enabled])
+
+useEffect(() => {
+  Moralis.onAccountChanged((account) => {
+      console.log(`Account changed to ${account}`)
+      if (account == null) {
+          window.localStorage.removeItem("connected")
+          deactivateWeb3()
+          console.log("Null Account found")
+      }
+  })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [])
 
   const isBlackNext = useRef(true); //black first
 
@@ -38,6 +62,21 @@ function App() {
     <div className="App">
       {/* Wrapper */}
       <div className="flex text-center my-[100px] mx-auto"> 
+      <button
+            onClick={async () => {
+                // await walletModal.connect()
+                await enableWeb3()
+                // depends on what button they picked
+                if (typeof window !== "undefined") {
+                    window.localStorage.setItem("connected", "injected")
+                    // window.localStorage.setItem("connected", "walletconnect")
+                }
+            }}
+            disabled={isWeb3EnableLoading}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+        >
+            Connect
+        </button>
         {/* Board */}
         <div className="my-0 mx-auto">
           {board.map((row: Array<string>, rowIndex: number) => {
--- prototypes/go-game-blockchain/fe/src/index.tsx
+++ prototypes/go-game-blockchain/fe/src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <MoralisProvider initializeOnMount={false}>
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
+  </MoralisProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
