0401e62e4beb6bf750eeb129d34d60ec48878820
easonyu0203 <breaking0203@gmail.com>
2022-12-11 09:09:12 +0800

    remove json

--- blockchain-auto-chess/Assets/Scripts/GameCore/WarSimulation.cs
+++ blockchain-auto-chess/Assets/Scripts/GameCore/WarSimulation.cs
@@ -4,13 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Army;
 using Army.AI;
-using Newtonsoft.Json;
-using Ultility;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Windows;
-using File = System.IO.File;
 
 namespace GameCore
 {
@@ -18,26 +13,8 @@ namespace GameCore
     {
         public UnityEvent<TeamColorTypes> WarOverUnityEvent;
 
-        private WarHistory _warHistory;
-
         public IEnumerator StartSimulation()
         {
-            _warHistory = new WarHistory();
-            _warHistory.InitBoardState = new AllSoldierPosInWar();
-            _warHistory.ActionInWars = new List<AllSoldierActionInWar>();
-            _warHistory.InitBoardState.Player1 = new Dictionary<string, Vector2Int>();
-            _warHistory.InitBoardState.Player2 = new Dictionary<string, Vector2Int>();
-            foreach (Soldier soldier in SoldierManager.Instance.Soldiers)
-            {
-                if (soldier.TeamColor == TeamColorTypes.Blue)
-                {
-                    _warHistory.InitBoardState.Player1.Add(soldier.NameInTeam, soldier.IndexPos);
-                }
-                else
-                {
-                    _warHistory.InitBoardState.Player2.Add(soldier.NameInTeam, soldier.IndexPos);
-                }
-            }
 
             float stepTime = GameManager.Instance.AnimationTimePerRound;
             bool isOver = false;
@@ -95,28 +72,6 @@ namespace GameCore
                 actions.Add(soldier, ai.DecideAction());
             }
 
-            AllSoldierActionInWar actionInWar = new AllSoldierActionInWar();
-            actionInWar.Player1 = new Dictionary<string, ActionJson>();
-            actionInWar.Player2 = new Dictionary<string, ActionJson>();
-            foreach(var( soldier, (action, payload)) in actions)
-            {
-                ActionJson actionJson = new ActionJson();
-                actionJson.ActionName = action.ToString();
-                actionJson.Payload = payload;
-                if (soldier.TeamColor == TeamColorTypes.Blue)
-                {
-                    actionInWar.Player1.Add(soldier.NameInTeam, actionJson);
-                }
-                else
-                {
-                    actionInWar.Player2.Add(soldier.NameInTeam, actionJson);
-                }
-            }
-            _warHistory.ActionInWars.Add(actionInWar);
-            string json = JsonConvert.SerializeObject(_warHistory);
-            File.WriteAllText("/Users/Ethan/Developer/Projects/college-projects/senior-project/war-history.txt", json);
-
-
             // execute actions
             foreach (var (soldier, (actionType, payload)) in actions.Select(x => (x.Key, x.Value)))
             {
@@ -135,31 +90,4 @@ namespace GameCore
         }
     }
 
-    [Serializable]
-    public class WarHistory
-    {
-        public AllSoldierPosInWar InitBoardState;
-        public List<AllSoldierActionInWar> ActionInWars;
-    }
-
-    [Serializable]
-    public class AllSoldierPosInWar
-    {
-        public Dictionary<string, Vector2Int> Player1;
-        public Dictionary<string, Vector2Int> Player2;
-    }
-
-    [Serializable]
-    public class AllSoldierActionInWar
-    {
-        public Dictionary<string, ActionJson> Player1;
-        public Dictionary<string, ActionJson> Player2;
-    }
-
-    [Serializable]
-    public class ActionJson
-    {
-        public string ActionName;
-        public Payload Payload;
-    }
 }
